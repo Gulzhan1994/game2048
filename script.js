381cac74e05edf85f89c07f535eee07784a21c45
Gulzhan <gulzhan0404@gmail.com>
2022-05-28 00:14:13 +0600

    fix

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ class Board {
         const fragment = document.createDocumentFragment();
         for(let i = 0; i < this.width * this.width; i++) {
             const square = document.createElement('div');
-            square.innerHTML = '';
-            square.className = 'cell';
+            square.innerHTML = '2';
+            square.className = 'grid-cell';
             fragment.appendChild(square);
             squares.push(square);
         }
