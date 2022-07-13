6cf73c154ec3a7c4b5c81e91e9b5357c5c8adb4f
Gulzhan <gulzhan0404@gmail.com>
2022-07-13 21:58:50 +0600

    added last changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-// import '../style.css';
-
 // // Нажимаем кнопку страт или новая игра
 // // В поле появляется цифры две цифры одинаковые;
 // // с помощью нажатием кнопки (право и влево, вверх и вниз) перемещаем цифры в поле игры;
 // // одинаковые цифры суммируя свои значения превращается в одну;
 // // Если в игровой поле невозможно совершить ход то тогда игра закончиться с поражением и выйдет окно с сообщением что Вы проиграли!
 
-// eslint-disable-next-line import/extensions
-// import { resultDisplay } from './board.js';
 import Board from './board.js';
+
+export const scoreDisplay = document.querySelector('.score');
+export const resultDisplay = document.querySelector('.result');
 
 // GameManager отвечает за общую игровую логику, начать игру, проследить не окончена ли игра, есть ли победитель и подобная логика
 class GameManager {
@@ -19,7 +18,8 @@ class GameManager {
     }
 
     init() {
-        this.board = importBoard;
+        // eslint-disable-next-line no-use-before-define
+        this.board = new Board();
         this.board.init();
         this.board.generateNewCell();
         this.board.generateNewCell();
@@ -30,39 +30,37 @@ class GameManager {
         if (event.key === 'ArrowUp') {
             this.board.movingColumn('up');
             this.board.combineColumn();
-            this.checkForGameOver();
             this.board.generateNewCell();
         } else if (event.key === 'ArrowDown') {
             this.board.movingColumn('down');
             this.board.combineColumn();
-            this.checkForGameOver();
             this.board.generateNewCell();
         } else if (event.key === 'ArrowRight') {
             this.board.movingRow('right');
             this.board.combineRow();
-            this.checkForGameOver();
             this.board.generateNewCell();
         } else if (event.key === 'ArrowLeft') {
             this.board.movingRow('left');
             this.board.combineRow();
-            this.checkForGameOver();
             this.board.generateNewCell();
         }
     }
 
     checkForGameOver() {
-        for (let i = 0; i < this.board.squares.length; i++) {
-            if (this.board.squares[i].getValue() === 2048) {
+        const zeros = 0;
+        for (let i = 0; i < this.squares.length; i++) {
+            if (this.squares[i].getValue() == 8) {
                 alert('You win!');
+                document.removeEventListener('keyup', this.clickControl.bind(this));
+            }
+
+            if (zeros === 0) {
+                resultDisplay.innerHTML = 'You lose!';
                 document.removeEventListener('keyup', this.clickControl.bind(this));
             }
         }
     }
 }
 
-const importBoard = new Board();
-
 const gameStart = new GameManager();
 gameStart.init();
-
-
